120245aa0c4aaa0e33ef9d9a39d33efda69eaa4f
Banklii <naybanksorryza123@gmail.com>
2022-03-21 05:18:33 +0700

    add content 3-6 + img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: false, noPause: true, showIndicators: true } }
+  ]
 })
 export class AppComponent {
   title = 'ng-simple';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule.forRoot(),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
